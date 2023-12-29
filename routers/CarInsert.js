33905c8b6a7a6fcd83f2aeac59ef27dbcaf4c427
yousseffayed139 <yousseffayed139@gmial.com>
2023-12-29 23:46:10 +0200

    car insert code

---
+++ routers/CarInsert.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+// Create a connection to the MySQL database
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '@Feezo139',
+  database: 'carrentalsystem',
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+// Define a route for inserting a new car (POST request)
+app.post('/car', (req, res) => {
+  const {
+    plate_id,
+    model,
+    production_year,
+    color,
+    photo,
+    category,
+    class_id,
+    office_id,
+    status,
+  } = req.body;
+
+  // Validate that required fields are provided
+  if (!plate_id || !model || !production_year || !color || !category || !class_id || !office_id || !status) {
+    res.status(400).json({ message: 'Missing required fields' });
+    return;
+  }
+
+  // Use the provided data to insert a new car into the database
+  const query =
+    'INSERT INTO car (plate_id, model, production_year, color, photo, category, class_id, office_id, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  db.query(
+    query,
+    [plate_id, model, production_year, color, photo, category, class_id, office_id, status],
+    (err, result) => {
+      if (err) {
+        console.error('Error inserting new car', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+        return;
+      }
+
+      res.json({ message: 'Car successfully inserted', carId: result.insertId });
+    }
+  );
+});
+
+// Additional routes or middleware can be added here...
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
