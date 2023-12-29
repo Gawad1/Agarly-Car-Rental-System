0596f84c0247ab796dc3a9c54525e959a31b2ccf
soffarr <msoffar321@gmail.com>
2023-12-29 23:25:39 +0200

    deletefinal

--- Data_Base/DDL.sql
+++ Data_Base/DDL.sql
@@ -1,4 +1,4 @@
--- Active: 1702938882061@@192.168.1.103@3306@gsff
+-- Active: 1702935863877@@127.0.0.1@3306@gsff
 
 CREATE TABLE customer (
     ssn INT PRIMARY KEY,
@@ -23,10 +23,10 @@ model VARCHAR(10) check(model in ('BMW','TOYOTA','SKODA','OPEL','NISSAN','HYNDAI
 production_year INT NOT NULL CHECK (production_year >= 1920 ),
 color varchar(10)  check (color in ('Blue','Green','Black','White','Red','Silver','Grey','Bronze')),
 photo varchar(255) ,
-category varchar(10) check (category in ('SUV','Sedan','CrossOver''Mini_Van')),
+category varchar(10) check (category in ('SUV','Sedan','CrossOver','Mini_Van')),
 class_id varchar(15) check (class_id in ('Economy','Mid-Range','Luxury')),
 office_id int not null,
-`status` varchar(15) check (`status` in ('Rented','Pending','Out-Of-Service','Active')),
+`status` varchar(15) check (`status` in ('Rented','Pending','Out-Of-Service','Active','NA')),
 Constraint fk_car_class 
 Foreign Key (class_id) REFERENCES class(class_id),
 constraint fk_car_office 
@@ -53,5 +53,13 @@ CREATE TABLE reservation (
     CONSTRAINT fk_reservation_customer FOREIGN KEY (ssn) REFERENCES customer(ssn),
     CONSTRAINT fk_reservation_car FOREIGN KEY (plate_id) REFERENCES car(plate_id)
 );
+CREATE TABLE servicelog (
+    service_id INT AUTO_INCREMENT PRIMARY KEY,
+    plate_id INT NOT NULL,
+    start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end-date TIMESTAMP ,
+    CONSTRAINT fk_service_car FOREIGN KEY (plate_id) REFERENCES car(plate_id)
+);
+
 
 
--- routers/deletecarRouter.js
+++ routers/deletecarRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.json());
 
@@ -16,9 +16,38 @@ db.connect((err) => {
     }
     console.log('Connected to the database');
   });
-app.post('/deletecar',(req,res) =>{
-    const {plate_id } = req.body
-})
+  app.post('/deletecar', (req, res) => {
+    const { plate_id } = req.body;
+  
+    // Check if the plate_id exists in the database
+    const checkQuery = 'SELECT * FROM Car WHERE Plate_id = ?';
+    db.query(checkQuery, [plate_id], (err, results) => {
+      if (err) {
+        console.error('Error querying the database:', err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
+  
+      // If no matching record is found, send a response indicating that the data doesn't exist
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'Data not found in the database' });
+      }
+  
+      // If the data exists, update the status of the car to indicate that it is no longer active
+      const updateStatusQuery = 'UPDATE Car SET Status = ? WHERE Plate_id = ?';
+      const newStatus = 'NA'; // You can choose an appropriate status
+      db.query(updateStatusQuery, [newStatus, plate_id], (err) => {
+        if (err) {
+          console.error('Error updating car status:', err);
+          return res.status(500).json({ message: 'Internal Server Error' });
+        }
+  
+        // Send a success response after updating the status
+        res.json({ message: 'Car status updated successfully' });
+      });
+    });
+  });
+  
+  
 
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
