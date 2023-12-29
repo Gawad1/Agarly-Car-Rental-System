dbb963346d9c91e3cf96bf3541dce4a58125d5d4
yousseffayed139 <yousseffayed139@gmial.com>
2023-12-29 05:13:18 +0200

    deleted the node folder

--- node/app.js
+++
@@ -1,8 +0,0 @@
-const funci= require('./logger.js');
-function hello(name){
-    console.log('hello '+name);
-}
-hello('gawad');
-funci.getmyvar('message');
-console.log(funci);
-console.log(funci.url);
--- node/logger.js
+++
@@ -1,6 +0,0 @@
-var url ='http://localhost'
-function getvar (messege){
- console.log(messege+' from logger');
-}
-module.exports.getmyvar = getvar;
-module.exports.url = url;
