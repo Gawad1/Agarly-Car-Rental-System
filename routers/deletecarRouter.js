73ffbe74cbc47616482bfa8adbb9b67581326993
yousseffayed139 <yousseffayed139@gmial.com>
2023-12-30 05:43:22 +0200

    finall bellah

--- routers/deletecarRouter.js
+++ routers/deletecarRouter.js
@@ -6,13 +6,13 @@ const deletecar = (db) => {
   // Create a connection to the MySQL database
   
 
-  router.post('/', (req, res) => {
-    const { plate_id } = req.body;
+  router.get('/:plateId', (req, res) => {
+    const plateId = req.params.plateId;
     
   
     // Check if the plate_id exists in the database
     const checkQuery = 'SELECT * FROM Car WHERE Plate_id = ?';
-    db.query(checkQuery, [plate_id], (err, results) => {
+    db.query(checkQuery, [plateId], (err, results) => {
       if (err) {
         console.error('Error querying the database:', err);
         return res.status(500).json({ message: 'Internal Server Error' });
@@ -26,7 +26,7 @@ const deletecar = (db) => {
       // If the data exists, update the status of the car to indicate that it is no longer active
       const updateStatusQuery = 'UPDATE Car SET Status = ? WHERE Plate_id = ?';
       const newStatus = 'NA'; // You can choose an appropriate status
-      db.query(updateStatusQuery, [newStatus, plate_id], (err) => {
+      db.query(updateStatusQuery, [newStatus, plateId], (err) => {
         if (err) {
           console.error('Error updating car status:', err);
           return res.status(500).json({ message: 'Internal Server Error' });
