364cd1ff447440a30efce4ee5a5d459184452cd5
yousseffayed139 <yousseffayed139@gmial.com>
2023-12-31 03:08:34 +0200

    cors

---
+++ BackEnd/routers/dbConfig.js
@@ -0,0 +1,9 @@
+const dbConfig = {
+    host: "192.168.1.8",
+    user: "useraa",
+    password: "passaa",
+    database: "gsff",
+  };
+  
+  module.exports = dbConfig;
+  
--- routers/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = {
-    host: "192.168.1.8",
-    user: "usera",
-    password: "passa",
-    database: "gsff",
-  };
-  
-  module.exports = dbConfig;
-  
