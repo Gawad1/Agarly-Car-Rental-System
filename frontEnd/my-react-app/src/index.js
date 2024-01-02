9b21b72f8e0fc45854c0a5ce8c5bd05fd4b21b83
yousseffayed139 <yousseffayed139@gmial.com>
2024-01-02 04:43:55 +0200

    dfdf

--- frontEnd/my-react-app/src/index.js
+++ frontEnd/my-react-app/src/index.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 // index.js
-
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app.tsx';
 
 const root = document.getElementById('root');
@@ -13,21 +12,3 @@ if (root) {
 } else {
   console.error('Root element not found');
 }
-=======
-  // index.js
-
-  import React from 'react';
-  import ReactDOM from 'react-dom';
-  import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
-  import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-  const root = document.getElementById('root');
-  if (root) {
-    const appRoot = createRoot(root);
-    appRoot.render(<App />);
-  } else {
-    console.error('Root element not found');
-  }
->>>>>>> Fayed_Final
