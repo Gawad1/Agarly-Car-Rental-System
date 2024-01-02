42aee341d1c29a04bbe3f0085f44456d54255b92
Gawad1 <gawad.hussein@gmail.com>
2024-01-02 07:15:37 +0200

    last update before branching

--- frontEnd/my-react-app/src/index.js
+++ frontEnd/my-react-app/src/index.js
@@ -1,6 +1,6 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app.tsx';
